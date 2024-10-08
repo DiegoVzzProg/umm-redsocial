d0bb0ed3367170e7209c3551e36d661a183fc6ea
programador3 <gama.programador3@outlook.com>
2024-10-08 10:51:56 -0600

    Tabla Usuario -> Commando para crear la tabla php artisan migrate
    :: Ver si se nececitan más campos

---
+++ database/migrations/2024_10_08_140817_create_usuario_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id_usuario');   // id_usuario INT Auto Increment
+            $table->string('nombre', 255);      // nombre VARCHAR(255)
+            $table->string('paterno', 255);     // paterno VARCHAR(255)
+            $table->string('materno', 255);     // materno VARCHAR(255)
+            $table->string('usuario', 50);      // usuario VARCHAR(50)
+            $table->text('contraseña');         // contraseña TEXT
+            $table->string('email', 254);       // email VARCHAR(254)
+            $table->text('telefono');           // telefono TEXT
+            $table->date('fecha_nacimiento');   // fecha_nacimiento DATE
+            $table->timestamp('fecha_registro')->useCurrent(); // fecha_registro TIMESTAMP con valor predeterminado actual
+            $table->char('sexo', 1);            // sexo CHAR(1)
+            $table->string('biografia', 200);   // biografia VARCHAR(200)
+            $table->string('rol', 50);          // rol VARCHAR(50)
+            $table->integer('seguidores');      // seguidores INT
+            $table->integer('sigue_a');         // sigue_a INT
+            $table->text('token_verificacion'); // token_verificacion TEXT
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuario');
+    }
+};
